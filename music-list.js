7d2766da71d9d00c1b85f6cde40b6acd6b9582c7
PedroKeita <82671771+PedroKeita@users.noreply.github.com>
2022-06-19 18:26:37 -0300

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,40 @@
+//Lista com todas as músicas
+
+let botamusica = [
+  {
+    name: "kickstart my heart",
+    artist: "Mötley Crüe",
+    img: "kickstartmyheart-1",
+    src: "kickstartmyheart-1"
+  },
+  {
+    name: "Hi-De-Ho",
+    artist: "Jack White",
+    img: "HiDeHo-2",
+    src: "HiDeHo-2"
+  },
+  {
+    name: "Corduroy Dreams",
+    artist: "Rex Orange County",
+    img: "corduroydreams-3",
+    src: "CorduroyDreams-3"
+  },
+  {
+    name: "Funk sonoplastia Vai dar Namoro",
+    artist: "Ayreless",
+    img: "rodrigo-4",
+    src: "funkvaidarnamoro-4"
+  },
+  {
+    name: "Glimpse of Us",
+    artist: "Joji",
+    img: "glimpseofus-5",
+    src: "GlimpseofUs-5"
+  },
+  {
+    name: "Can't Stop",
+    artist: "Red Hot Chili Peppers",
+    img: "cantstop-6",
+    src: "cantstop-6"
+  },
+];
